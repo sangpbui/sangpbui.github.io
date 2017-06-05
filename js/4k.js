335251899ae3f9d53757e599390d4c21f021e828
Khoa Le <kdl2483@truman.edu>
2017-06-05 18:13:47 -0500

    added the first story 'Leaving Kv' and new hover animation

--- js/4k.js
+++ js/4k.js
@@ -122,6 +122,35 @@ var path = [
 var covered = [];
 
 $(document).ready(function(){
+    // control gallery slider
+    var slideIndex = 1;
+    showDivs(slideIndex);
+
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+
+    function currentDiv(n) {
+        showDivs(slideIndex = n);
+    }
+
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        if (n > x.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = x.length}
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" w3-white", "");
+        }
+        x[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " w3-white";
+    }
+
+    // customize map progress
     var map;
    $(function () {
         map = new GMaps({ 
